e1578726e379cd08f4b476d0141251f9ad89ef2d
darrida <darrida.py@gmail.com>
2018-12-08 13:42:36 -0600

    check point

---
+++ src/Latin_Translator.java
@@ -0,0 +1,17 @@
+
+public class Latin_Translator {
+
+}
+
+
+/*
+ * Look at the following list of Latin words and their meanings:
+ * sinister = left
+ * dexter = right
+ * medium = center
+ * 
+ * Create a JavaFX application that translates the Latin words to English.
+ * The form should have three Buttons, one for each Latin word.
+ * When the user clicks a Button, the application should display the
+ * English translation in a Label.
+ */
