344f091566058cda95e74081cf35524cfc295be7
darrida <darrida.py@gmail.com>
2018-12-08 13:44:10 -0600

    check point

--- src/Latin_Translator.java
+++ src/Latin_Translator.java
@@ -14,4 +14,7 @@ public class Latin_Translator {
  * The form should have three Buttons, one for each Latin word.
  * When the user clicks a Button, the application should display the
  * English translation in a Label.
+ * 
+ * In the case of JavaFX errors: 
+ * https://stackoverflow.com/questions/22812488/using-javafx-in-jre-8-access-restriction-error
  */
